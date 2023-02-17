b36cfae1601d4596538ecb49aea045187eb530f4
winnyouri08 <118175027+winnyouri08@users.noreply.github.com>
2023-02-17 09:03:31 +0700

    science visualization part 1

---
+++ Cover_Page.js
@@ -0,0 +1,22 @@
+var Hai;
+
+Hai = "Hai juga";
+
+var a = 4;
+    while (a > 0.1){
+    a = a - 0.1
+    console.log(a)
+}
+    console.log("================")
+
+//// for
+var b = 4;
+for (var i=0;i<10;i++){
+    b = b - 0.1
+    console.log(b);
+}
+
+//=======Fungsi=============
+function f(x){
+    return x**2;
+}
